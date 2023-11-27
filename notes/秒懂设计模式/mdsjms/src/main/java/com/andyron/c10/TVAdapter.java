00a59341b3ec33673f7db82167dde0ad9f26f868
AndyRon <rongming.2008@163.com>
2023-11-27 12:27:20 +0800

    update

--- notes/秒懂设计模式/mdsjms/src/main/java/com/andyron/c10/TVAdapter.java
+++ notes/秒懂设计模式/mdsjms/src/main/java/com/andyron/c10/TVAdapter.java
@@ -2,8 +2,9 @@ package com.andyron.c10;
 
 /**
  * 电视机专属适配器类
+ * 类适配器（某个类的“专属适配器”），也就是在编码阶段已经将被匹配的设备与目标接口进行对接了也就是在编码阶段已经将被匹配的设备与目标接口进行对接了
  */
-public class TVAdapter extends TV implements TriplePin{
+public class TVAdapter extends TV implements TriplePin {
 
     @Override
     public void electrify(int l, int n, int e) {
